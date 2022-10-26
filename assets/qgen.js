0f101f22acca5f2ada7231fdc5bbebc7677f82b1
Sebastian Barrera <sebastianbarrera@live.com>
2022-10-26 10:12:37 -0400

    added js and css, fixed html

---
+++ assets/qgen.js
@@ -0,0 +1,43 @@
+// The following will generate the questions as objects within an array
+var questions = [
+    {
+      title: "Following are the JavaScript Data types:",
+      choices: ["Numbers", "String", "Boolean", "Object","Undefined","All of the above"],
+      answer: "All of the above"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "True or False, isNan function returns true if the argument is not a number; otherwise, it is false.",
+      choices: ["True", "False", "N/A"],
+      answer: "True"
+    },
+    {
+      title: "True or False, 'This' keyword refers to the object from where it was called.",
+      choices: ["True", "False","N/A"],
+      answer: "True"
+    },
+    {
+        title: "_______is a complete programming language. In contrast, _________ is a coded program that can be introduced to HTML pages. These two languages are not at all inter-dependent and are designed for different intent.",
+        choices: ["Javascript/Script", "BootStrap/Java", "Java/Javascript", "None of the listed"],
+        answer: "Java/Javascript"
+      },
+    {
+        title: "_____ checks only for equality in value, whereas ______is a stricter equality test and returns false if either the value or the type of the two variables are different.",
+        choices: ["!=,<=", "==,>=", "===,==", "==,==="],
+        answer: "==,==="
+     },  
+    {
+        title: "What is a valid Timer piece of code ?",
+        choices: ["TimeoutSet", "Interval(delay,function)", "clearSetTimeout(id)", "setInterval(function, delay)"],
+        answer: "setInterval(function, delay)"
+     },
+     {
+        title: " What is === operator?",
+        choices: ["Contains", "Almost Equivalant", "Partially Equal", "Strictly Equal"],
+        answer: "Strictly Equal"
+     },     
+  ];
